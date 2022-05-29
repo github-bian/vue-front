06489a24e4f22046fcfe7b37181826c33f52c7d4
边龙龙 <534893106@qq.com>
2022-05-29 15:01:35 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+     <ul >
+       <li v-for="item in users" :key="item.id">
+           {{item.name}}
+
+       </li>
+     </ul>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      users:[]
+    }
+  },
+  mounted() {
+    const api="http://localhost:3001/api/users"
+    axios.get(api).then(res=>{
+      this.users=res.data
+    })
+  },
+ 
+}
+</script>
+
+
