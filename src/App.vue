130d27d41a572776c8d6ea9d79342026271a4e67
bian <bian@biandeMacBook-Pro.local>
2022-06-28 00:14:27 +0800

    s

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <p>服了啊！</p>
-     <ul >
-       <li v-for="(item) in users" :key="item.id">
-           {{item.id}}-{{item.name}}
-       </li>
-     </ul>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <p>服了啊！!</p>
+    <ul>
+      <li v-for="item in users" :key="item.id">
+        {{ item.id }}-{{ item.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  name: 'App',
-  data(){
+  name: "App",
+  data() {
     return {
-      users:[]
-    }
+      users: [],
+    };
   },
   mounted() {
-    const api="/api/users"
-    axios.get(api).then(res=>{
-      console.log(res)
-      this.users=res.data
-    })
+    const api = "/api/users";
+    axios.get(api).then((res) => {
+      console.log(res);
+      this.users = res.data;
+    });
   },
- 
-}
+};
 </script>
 
 
